e5fecd6cf5616f048477f23890ccaf760ec4977c
surrealmemoir <surrealmemoir@users.noreply.github.com>
2015-02-04 11:42:04 -0500

    added cut and main

--- recorder.py
+++ recorder.py
@@ -47,3 +47,43 @@ def recordingReader(filename):
     f.close()
     print('End time', mytime.printI64(tmI64))
     print('Packet count ', msgcnt)
+
+def cutFile(date, begin, end, filename_from, filename_to):
+    from mytime import toI64
+    t1 = toI64(date, begin.hour, begin.minute, begin.second)
+    t2 = toI64(date, end.hour, end.minute, end.second)
+    
+    f1 = open(filename_from, 'rb')
+    f2 = open(filename_to, 'wb')
+    while True:
+        head = f1.read(2)
+        if not head:
+            break
+        sz, = struct.unpack('H', head)
+        rawtm = f1.read(8)
+        tmI64, = struct.unpack('Q', rawtm)
+        buf = f1.read(sz - 8)
+        if tmI64 <= t1:
+            continue
+        if tmI64 >= t2:
+            break
+        f2.write(head)
+        f2.write(rawtm)
+        f2.write(buf)
+    f1.close()
+    f2.close()    
+    
+if __name__ == '__main__':    
+    import argparse
+    import cme.settings
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--feed")   # F or O
+    parser.add_argument("--type")   # I or S or N
+    args = parser.parse_args()
+    
+    filename = cme.settings.recordPath(date.today(), args.feed, args.type)
+    addr = cme.settings.addrdict[args.feed][args.type]
+    
+    print('Feed: {} \nType: {} \nAddress: {}\nFile: {}'.format(args.feed, args.type, addr, filename))
+    mcRecorder(addr, filename)
